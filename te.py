ab9189fbc5791239d896f190256af1f65ba3bfba
Hugh Van <hvan021@gmail.com>
2014-07-23 17:18:33 +1200

    pep8 conform

--- te.py
+++ te.py
@@ -32,19 +32,19 @@ filemenu.add_command(label="Save as", accelerator='Shift+Ctrl+S')
 filemenu.add_separator()
 filemenu.add_command(label="Exit", accelerator='Alt+F4')
 
-#Edit menu - Undo, Redo, Cut, Copy and Paste
-editmenu.add_command(label="Undo",compound=LEFT,  image=undoicon, accelerator='Ctrl+Z')
-editmenu.add_command(label="Redo",compound=LEFT,  image=redoicon, accelerator='Ctrl+Y')
+# Edit menu - Undo, Redo, Cut, Copy and Paste
+editmenu.add_command(label="Undo", compound=LEFT, image=undoicon, accelerator='Ctrl+Z')
+editmenu.add_command(label="Redo", compound=LEFT, image=redoicon, accelerator='Ctrl+Y')
 editmenu.add_separator()
 editmenu.add_command(label="Cut", compound=LEFT, image=cuticon, accelerator='Ctrl+X')
-editmenu.add_command(label="Copy", compound=LEFT, image=copyicon,  accelerator='Ctrl+C')
-editmenu.add_command(label="Paste",compound=LEFT, image=pasteicon, accelerator='Ctrl+V')
+editmenu.add_command(label="Copy", compound=LEFT, image=copyicon, accelerator='Ctrl+C')
+editmenu.add_command(label="Paste", compound=LEFT, image=pasteicon, accelerator='Ctrl+V')
 editmenu.add_separator()
-editmenu.add_command(label="Find",underline= 0, accelerator='Ctrl+F')
+editmenu.add_command(label="Find", underline=0, accelerator='Ctrl+F')
 editmenu.add_separator()
 editmenu.add_command(label="Select All", underline=7, accelerator='Ctrl+A')
 
-#View menu -
+# View menu -
 showln = IntVar()
 showln.set(1)
 viewmenu.add_checkbutton(label="Show Line Number", variable=showln)
@@ -57,14 +57,14 @@ viewmenu.add_checkbutton(label="Highlight Current Line", onvalue=1, offvalue=0, 
 themesmenu = Menu(menubar, tearoff=0)
 viewmenu.add_cascade(label="Theme", menu=themesmenu)
 
-#we define a color scheme dictionary containg name and color code as key value pair
+# we define a color scheme dictionary containg name and color code as key value pair
 clrschms = {
     '1. Default White': 'FFFFFF',
-    '2. Greygarious Grey':'D1D4D1',
-    '3. Lovely Lavender':'E1E1FF' ,
+    '2. Greygarious Grey': 'D1D4D1',
+    '3. Lovely Lavender': 'E1E1FF',
     '4. Aquamarine': 'D1E7E0',
     '5. Bold Beige': 'FFF0E1',
-    '6. Cobalt Blue':'333AA',
+    '6. Cobalt Blue': '333AA',
     '7. Olive Green': '5B8340',
 }
 
